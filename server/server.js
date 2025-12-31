3110bbc9e3f1af68fc363a855d2db75a7114e692
Saara Kauser Shaik <shaiksaarakauser@gmail.com>
2025-12-31 21:36:41 +0530

    Serve static files and clean up server.js
    Updated server.js to serve static files from project root.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+// serves index.html, css, js, img, favicon.ico from project root
+app.use(express.static(path.join(__dirname, "../")));
 
-const PORT = 5000;
-const MONGO_URI = 'mongodb://127.0.0.1:27017/spotifyClone';
-
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
-    );
-  })
-  .catch(err => console.error(err));
+app.use("/api/auth", authRoutes);
